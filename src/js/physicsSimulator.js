fa3ead383b1c813babe2577251ab0d361daa8d84
Barry Becker <barrybecker4@gmail.com>
2021-05-15 04:16:01 -0700

    extract phaser scene.

--- src/js/physicsSimulator.js
+++ src/js/physicsSimulator.js
@@ -1,10 +1,8 @@
 import physicsOptions from "./physicsOptions.js";
-import PlanckWorld from "./PlanckWorld.js";
+import PhysicsSimulatorScene from "./PhysicsSimulatorScene.js";
 
 
 let game;
-let paused = false;
-
 
 window.onload = function() {
   const restartButton = document.getElementById("restartButton");
@@ -13,27 +11,27 @@ window.onload = function() {
   const pauseButton = document.getElementById("pauseButton");
   pauseButton.onclick = doPause;
 
-  doRestart();
+  initializePhaser();
 }
 
 function doRestart() {
   if (game) {
-    game.scene.start("PlayGame");
+    game.scene.start(PhysicsSimulatorScene.name, game.config);
   }
-  else initializePhaser();
 }
 
 function doPause() {
   const pauseButton = document.getElementById("pauseButton");
-  if (!paused) {
-    game.scene.pause("PlayGame");
-    pauseButton.innerText = "Resume";
-  }
-  else {
-    game.scene.resume("PlayGame");
+
+  if (game.scene.paused) {
+    game.scene.resume(PhysicsSimulatorScene.name);
     pauseButton.innerText = "Pause";
   }
-  paused = !paused;
+  else {
+    game.scene.pause(PhysicsSimulatorScene.name);
+    pauseButton.innerText = "Resume";
+  }
+  game.scene.paused = !game.scene.paused;
 }
 
 function initializePhaser() {
@@ -47,49 +45,8 @@ function initializePhaser() {
           width: 600,
           height: 600
       },
-      scene: PlayGame
+      scene: PhysicsSimulatorScene
     }
     game = new Phaser.Game(gameConfig);
     window.focus();
-}
-
-class PlayGame extends Phaser.Scene {
-  constructor(){
-    super("PlayGame");
-  }
-
-  create() {
-    const createGraphics = () => this.add.graphics();
-    this.world = new PlanckWorld(game.config.width, game.config.height, createGraphics);
-    this.world.createContent();
-
-    // creates a random box each 500ms, then restarts after a while.
-    this.tick = 0;
-    this.time.addEvent({
-        delay: 100,
-        callbackScope: this,
-        callback: function(){
-            const xPos = Phaser.Math.Between(100, game.config.width - 100);
-            this.world.createBox(xPos, -100, Phaser.Math.Between(20, 80), Phaser.Math.Between(20, 80), true, this.randomColor());
-            this.tick ++;
-            if (this.tick == 200) {
-                doRestart();
-            }
-        },
-        loop: true
-    });
-  }
-
-  randomColor() {
-    const color = new Phaser.Display.Color();
-    color.random();
-    color.saturate(-30).brighten(30);
-    return color;
-  }
-
-  update() {
-    if (paused) return;
-    const timeStep = 1 / 30;
-    this.world.update(timeStep);
-  }
 }
