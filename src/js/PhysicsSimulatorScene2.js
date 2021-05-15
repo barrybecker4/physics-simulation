7ce7babd188f21867cc359327248f6dc2cafa0a3
Barry Becker <barrybecker4@gmail.com>
2021-05-15 05:26:51 -0700

    switch between multiple simulations

--- src/js/PhysicsSimulatorScene2.js
+++ src/js/PhysicsSimulatorScene2.js
@@ -1,12 +1,12 @@
 import PlanckWorld from "./PlanckWorld.js";
 
-export default class PhysicsSimulatorScene extends Phaser.Scene {
+export default class PhysicsSimulatorScene2 extends Phaser.Scene {
 
-  static NAME = "PhysicsSimulatorScene";
-  static MAX_BLOCKS = 100;
+  static NAME = "PhysicsSimulatorScene2";
+  static MAX_BLOCKS = 300;
 
   constructor(){
-    super(PhysicsSimulatorScene.NAME);
+    super(PhysicsSimulatorScene2.NAME);
   }
 
   create() {
@@ -18,14 +18,14 @@ export default class PhysicsSimulatorScene extends Phaser.Scene {
     // creates a random box every short delay, then restarts after a while.
     this.tick = 0;
     this.time.addEvent({
-        delay: 400,
+        delay: 40,
         callbackScope: this,
         callback: function(){
             const xPos = Phaser.Math.Between(100, config.width - 100);
             this.world.createBox(xPos, -100, Phaser.Math.Between(20, 80), Phaser.Math.Between(20, 80), true, this.randomColor());
             this.tick ++;
-            if (this.tick == PhysicsSimulatorScene.MAX_BLOCKS) {
-                this.game.scene.start(PhysicsSimulatorScene.NAME, config);
+            if (this.tick == PhysicsSimulatorScene2.MAX_BLOCKS) {
+                this.game.scene.start(PhysicsSimulatorScene2.NAME, config);
             }
         },
         loop: true
--- src/js/physicsSimulator.js
+++ src/js/physicsSimulator.js
@@ -1,18 +1,29 @@
 import physicsOptions from "./physicsOptions.js";
 import PhysicsSimulatorScene from "./PhysicsSimulatorScene.js";
+import PhysicsSimulatorScene2 from "./PhysicsSimulatorScene2.js";
+
 
 let game;
 
 window.onload = function() {
+  getSimulationSelector().onchange = simulationSelectionChanged;
   getRestartButton().onclick = doRestart;
   getPauseButton().onclick = togglePause;
 
   initializePhaser();
 }
 
+
+function simulationSelectionChanged() {
+    const simulationSelector = getSimulationSelector();
+    game.scene.stop(game.config.currentScene);
+    game.config.currentScene = simulationSelector.options[ simulationSelector.selectedIndex ].value;
+    game.scene.start(game.config.currentScene);
+}
+
 function doRestart() {
   if (game) {
-    game.scene.start(PhysicsSimulatorScene.NAME, game.config);
+    game.scene.start(game.config.currentScene, game.config);
     doResume();
   }
 }
@@ -23,15 +34,19 @@ function togglePause() {
 }
 
 function doPause() {
-  game.scene.pause(PhysicsSimulatorScene.NAME);
+  game.scene.pause(game.config.currentScene);
   getPauseButton().innerText = "Resume";
   game.scene.paused = true;
 }
 
 function doResume() {
-  game.scene.resume(PhysicsSimulatorScene.NAME);
+  game.scene.resume(game.config.currentScene);
   getPauseButton().innerText = "Pause";
   game.scene.paused = false;
+}
+
+function getSimulationSelector() {
+    return document.getElementById("simulationSelector");
 }
 
 function getRestartButton() {
@@ -53,8 +68,9 @@ function initializePhaser() {
           width: 600,
           height: 600
       },
-      scene: PhysicsSimulatorScene
+      scene: [PhysicsSimulatorScene, PhysicsSimulatorScene2]
     }
     game = new Phaser.Game(gameConfig);
+    game.config.currentScene = PhysicsSimulatorScene.NAME;
     window.focus();
 }
