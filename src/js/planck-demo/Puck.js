1bcc87b0d5a5921f18cd8f2b455825d1f00c48ef
Barry Becker <barrybecker4@gmail.com>
2022-03-12 14:44:01 -0800

    trying to reset the puck to the middle

--- src/js/planck-demo/Puck.js
+++ src/js/planck-demo/Puck.js
@@ -3,21 +3,31 @@ export default class Puck {
 
     constructor(world) {
         this.world = world
-        this.body = world.createBody({
+        this.body = this.createBody()
+    }
+
+    createBody() {
+        const body = this.world.createBody({
             type: 'dynamic',
             position: planck.Vec2(0, 0),
             bullet: true,
             linearDamping: 0.1,
             angularDamping: 0.02
         })
-        this.body.createFixture(planck.Circle(1), {
+        body.createFixture(planck.Circle(1), {
             density: 0.25,
             restitution: 0.9,
             filterCategoryBits : 0x0004
         })
+        return body;
     }
 
-    destroy() {
-        this.world.destroyBody(this.body)
+    reset() {
+        //this.world.destroyBody(this.body)
+        this.body.setAwake(false)
+        this.body.setPosition(planck.Vec2(0, 0))
+        this.body.position = planck.Vec2(0, 0)
+        //this.body.setAwake(true)
+        //this.body = this.createBody()
     }
 }
--- src/js/planck-demo/air-hockey.js
+++ src/js/planck-demo/air-hockey.js
@@ -3,38 +3,36 @@ import Puck from "./Puck.js"
 import Paddle from "./Paddle.js"
 
 
-
 planck.testbed('AirHockey', testbed => {
     testbed.y = 0
     let canMove = false
     const force = 100
-    const pl = planck
-    const Vec2 = pl.Vec2
-    const world = pl.World()
+    const Vec2 = planck.Vec2
+    const world = planck.World()
 
     const table = new Table(world)
     const puck = new Puck(world)
     const paddle1 = new Paddle(Vec2(0, 16), world)
     const paddle2 = new Paddle(Vec2(0, -16), world)
 
-
+    /*
     function updatePosition(e) {
         if (canMove) {
           const vector = Vec2(e.movementX * force, -e.movementY * force)
-          paddle2.applyForce(vector, Vec2(paddle2.getPosition()))
+          //paddle2.applyForce(vector, Vec2(paddle2.getPosition()))
         }
-    }
+    }*/
 
     function handleContact(contact) {
         const fixtureA = contact.getFixtureA()
         const fixtureB = contact.getFixtureB()
         if (fixtureA == table.goal1Sensor) {
             alert('player1 scored')
-            puck.destroy()
+            puck.reset()
         }
         if (fixtureA == table.goal2Sensor) {
             alert('player2 scored')
-            puck.destroy()
+            puck.reset()
         }
     }
 
@@ -42,9 +40,9 @@ planck.testbed('AirHockey', testbed => {
         canMove = document.pointerLockElement === document.body ? true : false
     }
 
-    document.addEventListener('pointerlockchange', () => unlock())
-    window.addEventListener('mousemove', (e) => updatePosition(e))
-    document.body.addEventListener('click', () => document.body.requestPointerLock())
+    //window.addEventListener('mousemove', (e) => updatePosition(e))
+    window.addEventListener('mousedown', (e) => canMove = true)
+    window.addEventListener('mouseup', (e) => canMove = false)
     world.on('begin-contact', (e) => handleContact(e))
 
     return world
